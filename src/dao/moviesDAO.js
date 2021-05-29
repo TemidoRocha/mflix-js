850c2b9e149e3ce1673f8e8ae9c72fd4fe364f4a
TemidoRocha <manueltemidorocha@gmail.com>
2021-05-29 08:30:36 +0100

    error handling ticket completed

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -337,6 +337,7 @@ export default class MoviesDAO {
 
       return result
     } catch (e) {
+      console.log(e.toString())
       /**
       Ticket: Error Handling
 
@@ -346,7 +347,13 @@ export default class MoviesDAO {
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
-      console.error(`Something went wrong in getMovieByID: ${e}`)
+      if (
+        e.toString() ===
+        "Error: Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+      ) {
+        console.error(`Something went wrong in getMovieByID: ${e}`)
+        return null
+      }
       throw e
     }
   }
